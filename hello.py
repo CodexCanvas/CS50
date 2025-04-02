5bbb59302d1de7f70fbae6c9b1efaa38fdbd9668
CodexCanvas <Prattz440@Gmail.com>
2025-04-01 22:15:09 -0300

    01

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 # Ask user for their name and greet them
-name = input("What is your name? ")
+name = input("What is your name? ").strip().title()  # Strip leading and trailing whitespace
+
+#split user input into first and last name
+first_name, last_name = name.split()  # Split the name into first and last names
+
+
 
 # Say hello to the user
-print(f"Hello, {name}!")  # f-string for formatted string literals
+print(f"Hello, {first_name}!")  # f-string for formatted string literals
